0a570e6b93273debf450fdffd129971720225a91
hugh <hughebeling@hotmail.com>
2026-09-07 22:16:48 +1000

    style: clean vendored driver whitespace

--- lib/crowpanel_epaper/src/EPD.cpp
+++ lib/crowpanel_epaper/src/EPD.cpp
@@ -18,26 +18,26 @@ void Paint_NewImage(uint8_t *image,uint16_t Width,uint16_t Height,uint16_t Rotat
 {
 	Paint.Image = 0x00;
 	Paint.Image = image;
-	Paint.color = Color;  
+	Paint.color = Color;
 	Paint.widthMemory = Width;
-	Paint.heightMemory = Height;  
+	Paint.heightMemory = Height;
 	Paint.widthByte = (Width % 8 == 0)? (Width / 8 ): (Width / 8 + 1);
-	Paint.heightByte = Height;     
+	Paint.heightByte = Height;
 	Paint.rotate = Rotate;
-	if(Rotate==0||Rotate==180) 
+	if(Rotate==0||Rotate==180)
 	{
 		Paint.width=Height;
 		Paint.height=Width;
-	} 
-	else 
+	}
+	else
 	{
 		Paint.width = Width;
 		Paint.height = Height;
 	}
-}				 
+}
 
 /*******************************************************************
-    函数说明：清空缓冲区 
+    函数说明：清空缓冲区
     接口说明：Color  像素点颜色参数
     返回值：  无
 *******************************************************************/
@@ -45,10 +45,10 @@ void Paint_Clear(uint8_t Color)
 {
 	uint16_t X,Y;
 	uint32_t Addr;
-  for(Y=0;Y<Paint.heightByte;Y++) 
+  for(Y=0;Y<Paint.heightByte;Y++)
 	{
-    for(X=0;X<Paint.widthByte;X++) 
-		{   
+    for(X=0;X<Paint.widthByte;X++)
+		{
       Addr=X+Y*Paint.widthByte;//8 pixel =  1 byte
       Paint.Image[Addr]=Color;
     }
@@ -67,8 +67,8 @@ void Paint_SetPixel(uint16_t Xpoint,uint16_t Ypoint,uint16_t Color)
 {
 	uint16_t X, Y;
 	uint32_t Addr;
-	uint8_t Rdata;		
-    switch(Paint.rotate) 
+	uint8_t Rdata;
+    switch(Paint.rotate)
 		{
 				case 0:
 					if(Xpoint>=396)
@@ -109,12 +109,12 @@ void Paint_SetPixel(uint16_t Xpoint,uint16_t Ypoint,uint16_t Color)
 		Addr=X/8+Y*Paint.widthByte;
     Rdata=Paint.Image[Addr];
     if(Color==BLACK)
-    {    
+    {
 			Paint.Image[Addr]=Rdata&~(0x80>>(X % 8)); //将对应数据位置0
 		}
     else
 		{
-      Paint.Image[Addr]=Rdata|(0x80>>(X % 8));   //将对应数据位置1  
+      Paint.Image[Addr]=Rdata|(0x80>>(X % 8));   //将对应数据位置1
 		}
 }
 
@@ -129,7 +129,7 @@ void Paint_SetPixel(uint16_t Xpoint,uint16_t Ypoint,uint16_t Color)
     返回值：  无
 *******************************************************************/
 void EPD_DrawLine(uint16_t Xstart,uint16_t Ystart,uint16_t Xend,uint16_t Yend,uint16_t Color)
-{   
+{
 	uint16_t Xpoint, Ypoint;
 	int dx, dy;
 	int XAddway,YAddway;
@@ -175,12 +175,12 @@ void EPD_DrawRectangle(uint16_t Xstart,uint16_t Ystart,uint16_t Xend,uint16_t Ye
 	uint16_t i;
     if (mode)
 			{
-        for(i = Ystart; i < Yend; i++) 
+        for(i = Ystart; i < Yend; i++)
 				{
           EPD_DrawLine(Xstart,i,Xend,i,Color);
         }
       }
-		else 
+		else
 		 {
         EPD_DrawLine(Xstart, Ystart, Xend, Ystart, Color);
         EPD_DrawLine(Xstart, Ystart, Xstart, Yend, Color);
@@ -341,7 +341,7 @@ void EPD_ShowNum(uint16_t x,uint16_t y,uint32_t num,uint16_t len,uint16_t size1,
 			{
 				EPD_ShowChar(x+(size1/2+m)*t,y,'0',size1,color);
       }
-			else 
+			else
 			{
 			  EPD_ShowChar(x+(size1/2+m)*t,y,temp+'0',size1,color);
 			}
@@ -360,7 +360,7 @@ void EPD_ShowNum(uint16_t x,uint16_t y,uint32_t num,uint16_t len,uint16_t size1,
 *******************************************************************/
 
 void EPD_ShowFloatNum1(uint16_t x,uint16_t y,float num,uint8_t len,uint8_t pre,uint8_t sizey,uint8_t color)
-{         	
+{
 	uint8_t t,temp,sizex;
 	uint16_t num1;
 	sizex=sizey/2;
@@ -384,7 +384,7 @@ void EPD_ShowFloatNum1(uint16_t x,uint16_t y,float num,uint8_t len,uint8_t pre,u
 
 //GUI显示秒表
 void EPD_ShowWatch(uint16_t x,uint16_t y,float num,uint8_t len,uint8_t pre,uint8_t sizey,uint8_t color)
-{         	
+{
 	uint8_t t,temp,sizex;
 	uint16_t num1;
 	sizex=sizey/2;
--- lib/crowpanel_epaper/src/EPD.h
+++ lib/crowpanel_epaper/src/EPD.h
@@ -13,18 +13,18 @@ typedef struct {
 	uint16_t rotate;
 	uint16_t widthByte;
 	uint16_t heightByte;
-	
+
 }PAINT;
 extern PAINT Paint;
 
-//定义E-Paper显示方向 
+//定义E-Paper显示方向
 /*******************
 Rotaion:0-0度方向
 Rotaion:90-90度方向
 Rotaion:180-180度方向
 Rotaion:270-270度方向
 *******************/
-#define Rotation 180  
+#define Rotation 180
 
 
 void Paint_NewImage(uint8_t *image,uint16_t Width,uint16_t Height,uint16_t Rotate,uint16_t Color); 					 //创建画布控制显示方向
@@ -36,7 +36,7 @@ void EPD_DrawCircle(uint16_t X_Center,uint16_t Y_Center,uint16_t Radius,uint16_t
 void EPD_ShowChar(uint16_t x,uint16_t y,uint16_t chr,uint16_t size1,uint16_t color);                          //显示字符
 void EPD_ShowString(uint16_t x,uint16_t y,const char *chr,uint16_t size1,uint16_t color);                        //显示字符串
 void EPD_ShowNum(uint16_t x,uint16_t y,uint32_t num,uint16_t len,uint16_t size1,uint16_t color);                 //显示数字
-void EPD_ShowPicture(uint16_t x,uint16_t y,uint16_t sizex,uint16_t sizey,const uint8_t BMP[],uint16_t Color);		//显示图片 
+void EPD_ShowPicture(uint16_t x,uint16_t y,uint16_t sizex,uint16_t sizey,const uint8_t BMP[],uint16_t Color);		//显示图片
 void EPD_ClearWindows(uint16_t xs,uint16_t ys,uint16_t xe,uint16_t ye,uint16_t color);
 void EPD_ShowFloatNum1(uint16_t x,uint16_t y,float num,uint8_t len,uint8_t pre,uint8_t sizey,uint8_t color);
 void EPD_ShowWatch(uint16_t x,uint16_t y,float num,uint8_t len,uint8_t pre,uint8_t sizey,uint8_t color);
